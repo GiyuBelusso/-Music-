377d11fca49564351e4e6f4838bd263d4bf7a6bc
GiyuBelusso <lirabelussoanderson@gmail.com>
2025-02-17 17:13:14 -0300

    ajustes 2

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import "dotenv/config";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+const URL =
+  NODE_ENV === "development" ? "https://music-4h5b.onrender.com/api" : "/api";
 
 const resArtist = await axios.get(`${URL}/artists`); //this is the same as the insertMany method
 const resSongs = await axios.get(`${URL}/songs`);
