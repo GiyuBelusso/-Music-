592c2fd3b7b4e43a9a6509246016cc1856ffff3c
GiyuBelusso <lirabelussoanderson@gmail.com>
2025-02-17 16:42:23 -0300

    Ajustes

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -4,21 +4,31 @@
 import { db } from "./connect.js";
 import express from "express";
 import cors from "cors";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors()); //cross origin resource sharing middleware
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("just working on /songs and /artists");
 });
 
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray());
 });
-app.get("/songs", async (req, res) => {
+
+app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
 
-const URL = "http://localhost:5000";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
 
 const resArtist = await axios.get(`${URL}/artists`); //this is the same as the insertMany method
 const resSongs = await axios.get(`${URL}/songs`);
